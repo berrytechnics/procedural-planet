a01c40c22428f7ac06c1b79fbd8a3a29bce8343b
Kyle Berry <kyle@libiny.com>
2023-11-06 20:54:58 -0600

    hard-code ocean

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/no-unknown-property */
-import { Canvas, } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei';
 import Planet from './Planet';
+import { useEffect, useRef } from 'react';
 function App ()
 {
   return ( <div style={ { width: window.innerWidth, height: window.innerHeight } }>
     <Canvas camera={ { position: [ 0, 100, 250 ] } } style={ { height: window.innerHeight, width: window.innerWidth } }>
-      <directionalLight intensity={ 1 } />
+      <directionalLight intensity={ 1 } position={ [ -100, 100, 0 ] } />
       <ambientLight intensity={ 1 } />
       <OrbitControls />
       <Planet />
--- src/Planet.jsx
+++ src/Planet.jsx
@@ -6,26 +6,34 @@ import helpers from "./helpers";
 export default function Planet ()
 {
     const planetRef = useRef();
+    const groupRef = useRef();
     useFrame( () =>
     {
-        planetRef.current.rotation.y += 0.005;
+        groupRef.current.rotation.y += 0.005;
     } )
     useEffect( () =>
     {
-        helpers.generate3DPerlinTerrain( planetRef, 2, 20 )
-        helpers.generate3DPerlinTerrain( planetRef, 5, 20 )
+        helpers.generate3DPerlinTerrain( planetRef, 1, 20 );
+        helpers.generate3DPerlinTerrain( planetRef, .9, 15 );
+        helpers.generate3DPerlinTerrain( planetRef, .1, .1 );
         helpers.generateFBM3DTerrian( planetRef, {
             seed: Math.random(),
-            amplitude: 4,
-            scale: 25,
+            amplitude: 2,
+            scale: 10,
             octaves: 12,
             persistance: .5,
             lacunarity: 10,
             redistribution: 4
         } );
     }, [] );
-    return <mesh ref={ planetRef } >
-        <icosahedronGeometry args={ [ 64, 128 ] } />
-        <meshStandardMaterial color={ 'grey' } />
-    </mesh>
+    return <group ref={ groupRef }>
+        <mesh ref={ planetRef } >
+            <icosahedronGeometry args={ [ 64, 128 ] } />
+            <meshStandardMaterial color={ 'grey' } />
+        </mesh>
+        <mesh>
+            <sphereGeometry args={ [ 64.3, 24, 24 ] } />
+            <meshStandardMaterial color="#224488" />
+        </mesh>
+    </group>
 }
